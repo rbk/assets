f75195c211590562d2c4422e8fff543f27231f32
Richard <richard.be.jamin@gmail.com>
2016-01-07 01:36:17 -0600

    users and nav

--- js/app.js
+++ js/app.js
@@ -1,6 +1,17 @@
+console.log( Object.keys( window ) );
+console.log( Object.keys( document ) );
+
+
 jQuery(document).ready(function($){
 
-	console.log( Object.keys( window ) );
-	console.log( Object.keys( document ) );
 
+	// top-nav checker
+	var url = location.origin + location.pathname;
+	$('#top-nav a').each(function(){
+		var link = $(this).attr('href');
+		if( link.match(url) ){
+			$(this).addClass('active');
+		}
+	});
+	
 });
